5e5b0f1dff6e2ed0e9ca5a038d3c234b93d13e84
ashmick <ashley.mcneill2014@gmail.com>
2022-11-27 18:33:25 -0500

    Added for loops for restaurants, transportation, destination, & entertainment

--- main.py
+++ main.py
@@ -1,30 +1,54 @@
 destinations= ["Aruba", "New York City", "California", "Hawaii"]
 restaurants= ["Lima Bistro", "Cafe Neo", "Indian Bistro", "Tastee's Delights"]
-transportation= ["bus", "train", "cab", "boat"]
-entertainment= ["Snorkel with fish", "Hiking", "Escape Room", "Siteseeing"]
+transportations= ["bus", "train", "cab", "boat"]
+entertainments= ["Snorkel with fish", "Hiking", "Escape Room", "Siteseeing"]
 
+print("We're so happy that you chose us to help you plan your next trip. If you got the cash we have the plans! ")
+    
+for destination in destinations:
+    answer_to_choice= input(print(f'The location we have chose for you is {destination}. Would you like that choice y/n? '))
+    if answer_to_choice == "y":
+        print ("Great choice I think you will love it there! Let's move on to your restaurant recommendations!")
+        break
+    elif answer_to_choice == "n":
+        print ("No worries! We have a few other options for you! Let's try another one! ")
+        continue
+    
+    
+for restaurant in restaurants:
+    answer_to_restaurant= input(print(f'The restaurant we have as an option is {restaurant}. Would you like that choice y/n? '))
+    if answer_to_restaurant == "y":
+        print ("Great choice again. I think the food is wonderful there. Let's move on to your transportation options!")
+        break
+    elif answer_to_restaurant == "n":
+        print ("No worries! We have a few other options for you! Let's try another one! ")
+        continue
+    
+for transportation in transportations:
+    answer_to_transportation= input(print(f'The transportation option we suggest as an option is {transportation}. Would you like that choice y/n? '))
+    if answer_to_transportation == "y":
+        print ("Great choice again. The experience will be great. Let's move on to your entertainment options!")
+        break
+    elif answer_to_transportation == "n":
+        print ("No worries! We have a few other options for you! Let's try another one! ")
+        continue
+    
+    
+for entertainment in entertainments:
+    answer_to_entertainment= input(print(f'The transportation option we suggest as an option is {entertainment}. Would you like that choice y/n? '))
+    if answer_to_entertainment == "y":
+        print ("Great choice again. The fun you have will be amazing! Let's confirm your choices!")
+        break
+    elif answer_to_transportation == "n":
+        print ("No worries! We have a few other options for you! Let's try another one! ")
+        continue
+    
+    
 
+    
 
-
-# (5 points): As a developer, I want to make at least three commits with descriptive messages. 
-
-# (5 points): As a developer, I want to store my destinations, restaurants, mode of transportation, 
-# and entertainment selections in their own separate lists. 
-
-# (5 points): As a user, I want a destination to be randomly selected for my day trip. 
-
-# (5 points): As a user, I want a restaurant to be randomly selected for my day trip. 
-
-# (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip. 
-
-# (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip. 
-
-# (15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of 
-# transportation, and/or form of entertainment if I don’t like one or more of those things. 
-
-# (10 points): As a user, I want to be able to confirm that my day trip is “complete” once I like all
-# of the random selections. 
-
-# (10 points): As a user, I want to display my completed trip in the console. 
-
-# (5 points): As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
+    
+    
+    
+            
+            
